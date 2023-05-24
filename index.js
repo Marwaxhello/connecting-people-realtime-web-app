3c6e569f2fb2558b6cc7ae1300e5fedb1b3a3692
Maaike Verplancke <maaike.verplancke@hva.nl>
2023-05-24 15:55:58 +0200

    Vorige project erin gezet
    en ejs aan de modules toegevoegd

--- index.js
+++ index.js
@@ -1,45 +1,89 @@
+// Express uit de nodemodules map
+import { log } from "console";
 import express from "express";
+import { ppid } from "process";
 
-const url = "https://whois.fdnd.nl/api/v1/squad/";
+const url = "https://api.vinimini.fdnd.nl/api/v1/producten"; // URL naar Json data
+const url2 = "https://api.vinimini.fdnd.nl/api/v1";
 
-// Maak een nieuwe express app
+// Maak een nieuwe express app aan
 const app = express();
 
-// Stel in hoe we express gebruiken
+//  Stel in hoe we express gebruiken
 app.set("view engine", "ejs");
 app.set("views", "./views");
+
+// Gebruik maken van de "public" map
 app.use(express.static("public"));
 
 // Maak een route voor de index
+
 app.get("/", (request, response) => {
-  console.log(request.query.squad);
+  response.render("index");
+});
 
-  let slug = request.query.squad || "squad-b-2022";
-  let orderBy = request.query.orderBy || "surname";
-  let squadUrl = url + slug + "?orderBy=" + orderBy + "&direction=ASC";
-
-  fetchJson(squadUrl).then((data) => {
-    response.render("index", data);
+app.get("/alleproducten", (request, response) => {
+  let productenUrl = url;
+  fetchJson(productenUrl).then((data) => {
+    response.render("alleproducten", data);
   });
 });
 
-// app.get('/members', (request, response) => {
-//   response.send('Joepie!!')
-// })
+app.get("/inloggen", (request, response) => {
+  response.render("inloggen");
+});
+
+app.get("/ingelogd", (request, response) => {
+  response.render("ingelogd");
+});
+
+app.get("/kalender", (request, response) => {
+  response.render("kalender");
+});
+
+app.get("/speciaalvoormini", (request, response) => {
+  response.render("speciaalvoormini");
+});
+
+app.get("/test", (request, response) => {
+  response.render("test");
+});
+
+// Dit is de data uit de notitie api // Met dit stuk code willen we de data roepen uit de notitie api
+app.post("/kalender", function (req, res, next) {
+  const baseurl = "https://api.vinimini.fdnd.nl/api/v1/";
+  const url = `${baseurl}`;
+  req.body.afgerond = false;
+  req.body.persoonId = "clemozv3c3eod0bunahh71sx7";
+  req.body.datum = req.body.datum + ":00Z";
+  req.body.herinnering = [req.body.herinnering + ":00Z"];
+  console.log(req.body);
+
+  //Nieuwe notite kunnen maken
+  postJson(url + "/notities", req.body).then((data) => {
+    console.log(JSON.stringify(data));
+    let newNotitie = { ...req.body };
+
+    if (data.success) {
+      res.redirect("/kalender");
+      // /(shlash) Verwijst naar de homepage
+    } else {
+      const errormessage = `${data.message}: Mogelijk komt dit door de slug die al bestaat.`;
+      const newdata = { error: errormessage, values: newData };
+
+      res.render("index", newdata);
+    }
+  });
+});
 
 // Stel het poortnummer in en start express
-app.set("port", process.env.PORT || 7272);
+app.set("port", process.env.PORT || 8000);
 app.listen(app.get("port"), function () {
   console.log(`Application started on http://localhost:${app.get("port")}`);
 });
 
-/**
- * Wraps the fetch api and returns the response body parsed through json
- * @param {*} url the api endpoint to address
- * @returns the json response from the api endpoint
- */
-async function fetchJson(url) {
-  return await fetch(url)
+async function fetchJson(url, payload = {}) {
+  return await fetch(url, payload)
     .then((response) => response.json())
     .catch((error) => error);
 }
